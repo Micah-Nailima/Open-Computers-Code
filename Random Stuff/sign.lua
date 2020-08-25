e3e4b2ca93bff7137c6dd9139658ffe906726932
Micah <Micahsng@gmail.com>
2020-08-25 10:43:48 -0700

    big brain i made a sign

---
+++ Random Stuff/sign.lua
@@ -0,0 +1,13 @@
+local com = require('component')
+local gpu = com.gpu
+local screen = com.screen
+
+gpu.setResolution(28, 4)
+local width, height = 30, 4
+local text = "Manufacturing and Storage"
+
+while true do
+    os.execute("cls")  
+    gpu.set(width / 2 - #text / 2, height / 2, text)
+    os.sleep(100)
+end
